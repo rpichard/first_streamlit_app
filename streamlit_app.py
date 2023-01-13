ceba45089d16f7ac929ef808de72c204de77e7cc
Romain Pichard <122538923+rpichard@users.noreply.github.com>
2023-01-13 12:01:21 +0100

    Update streamlit_app.py
    correct typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-import request
+import requests
 fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
