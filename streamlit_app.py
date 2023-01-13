fa129136d7f5ba047923e603529145d01697c23c
Romain Pichard <122538923+rpichard@users.noreply.github.com>
2023-01-13 12:04:43 +0100

    Update streamlit_app.py
    add missing parenthese

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,6 @@ streamlit.dataframe(fruits_to_show)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.header("Fruityvice Fruit Advice!")
-streamlit.text(fruityvice_response.json)
+streamlit.text(fruityvice_response.json())
 
 
